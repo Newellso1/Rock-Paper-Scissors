345594b487b8def956925ba9e8971eb76376c264
Sam <samnewell91@gmail.com>
2023-03-18 19:28:35 +0000

    Final version (before adding visuals)
    A game of Rock, Paper, Scissors played in the console. I've added a round counter along with a score counter so that when the game is over a message is logged the details who won along with the final score. I also added a reset function after noticing that the score remained the same after the game was over.

--- script.js
+++ script.js
@@ -3,48 +3,81 @@
 const choices = ['Rock', 'Paper', 'Scissors'];
 
 // Creating the variables
+
 let playerChoice
 let computerChoice
+let playerScore = 0;
+let computerScore = 0;
+let roundNumber = 0;
 
+// Selecting computer choice
 
-// Selecting computer choice3
 function computerPlay() {
     return choices[Math.floor(Math.random()*choices.length)]
+}
+// Game instructions :
+
+console.log("game() to start a new round \n reset() to reset the score");
+
+// Function to reset the scores before staring a new game
+
+function reset() {
+    playerScore = 0;
+    computerScore = 0;
 }
 
 // Function to play a round
 
 function round(playerChoice, computerChoice) {
-    
+    roundNumber;
+    playerScore;
+    computerScore;
     playerChoice = prompt('Rock, Paper or Scissors?');
     computerChoice = computerPlay();
 
+// Player beats computer with Rock
     if (computerChoice.toLowerCase() == "rock" && playerChoice.toLowerCase() == 'paper') {
         console.log(`You win, the computer chose ${computerChoice}`);
+        playerScore++
+        roundNumber++;
         return ("Well done, you win");
-    
+ // Player beats computer with Paper   
     } else if (computerChoice.toLowerCase() == 'paper' && playerChoice.toLowerCase() == 'scissors') {
         console.log(`You win, the computer chose ${computerChoice}`);
+        playerScore++
+        roundNumber++;
         return ("Well done, you win");
-       
+// Player beats computer with Scissors  
     } else if (computerChoice.toLowerCase() == 'scissors' && playerChoice.toLowerCase() == 'rock') {
         console.log(`You win, ${playerChoice} beats ${computerChoice}`);
+        playerScore++;
+        roundNumber++;
         return ("Well done, You Win");
-    
+// Both Computer and Player make the same Choice
     } else if (computerChoice.toLowerCase() == playerChoice.toLowerCase()) {
         console.log("Its a draw");
-        return ("Its a Draw")
-   
+        roundNumber++;
+        return ("Its a Draw");
+// All other results are a loss
     } else {
-        console.log(`You lost, ${computerChoice} beats ${playerChoice}`)
+        console.log(`You lost, ${computerChoice.toLowerCase()} beats ${playerChoice.toLowerCase()}`)
+        computerScore++
+        roundNumber++;
         return ("You lost")
     }
     }
 
-    // Running a game of 5 rounds
+    // Running a game of 5 rounds and display final score
 
     function game() {
         for (let i = 0; i < 5; i++) {
-            round(playerChoice, computerChoice)
+            round(playerChoice, computerChoice);
+        }
+
+        if (roundNumber == 5 && playerScore > computerScore) {
+            console.log(`You won, the score was You ${playerScore} - CPU ${computerScore}`);
+        } else {
+            console.log(`You lost, the score was CPU ${computerScore} : You ${playerScore}`)
         }
     }
+    
